04ff804652b6b7a4bcc879a2f138bf240212ed67
ilhomjon003 <ilhomjonisoqjonov2@gmail.com>
2023-08-16 07:53:40 +0500

    tweets

--- src/pages/User/Profile/index.tsx
+++ src/pages/User/Profile/index.tsx
@@ -1,9 +1,12 @@
-import { useEffect } from "react";
-import { useNavigate, useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import StyledProfile from "./style";
+import { IUser } from "../../../interfaces";
+import { useAppDispatch } from "../../../hooks";
 import PageTitle from "../../../components/Title";
 import { useGetUserInfoQuery } from "../../../service";
 import { signout } from "../../../store/reducer/AuthSlice";
-import { useAppDispatch } from "../../../hooks";
+
 type TUsername = {
   username?: string | any;
 };
@@ -11,11 +14,12 @@ const Profile = () => {
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
   const { username }: TUsername = useParams();
+  const [userData, setUserData] = useState<IUser | null>(null);
   const { data, error, isLoading } = useGetUserInfoQuery(username);
 
   useEffect(() => {
     if (data && !isLoading) {
-      console.log(data);
+      setUserData(data.data);
     }
   }, [data]);
 
@@ -30,11 +34,19 @@ const Profile = () => {
     navigate("/");
   };
   return (
-    <div>
-      <PageTitle>Profile</PageTitle>
+    <StyledProfile>
+      <PageTitle className="profile__header">
+        <Link to="/" className="back__icon">
+          {"<-"}
+        </Link>{" "}
+        <div>
+          {userData?.name} <br />
+          <p className="user__tweets__indicator">9 tweets</p>
+        </div>
+      </PageTitle>
 
       <button onClick={handleSignOut}>Sign out</button>
-    </div>
+    </StyledProfile>
   );
 };
 
